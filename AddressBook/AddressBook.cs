f0310bfd6e4dd47690a80ca960eda3c5e92fd00a
Faiz <faiz.anwar4749@gmail.com>
2020-10-06 02:13:39 +0530

    [Faiz] Refactor: Adding a contact

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        private Dictionary<string, Contact> addressBook = new Dictionary<string, Contact>();
+        public void AddContact(string firstName, string lastName, string address, string city, string state, int zip, long phoneNumber, string email)
+        {
+            Contact contact = new Contact();
+            contact.FirstName = firstName;
+            contact.LastName = lastName;
+            contact.Address = address;
+            contact.City = city;
+            contact.State = state;
+            contact.Zip = zip;
+            contact.PhoneNumber = phoneNumber;
+            contact.Email = email;
+            addressBook.Add(contact.FirstName, contact);
+        }
+        public void ViewContact()
+        {
+            foreach (KeyValuePair<string, Contact> item in addressBook)
+            {
+                Console.WriteLine("First Name: " + item.Value.FirstName);
+                Console.WriteLine("Last Name: " + item.Value.LastName);
+                Console.WriteLine("Address: " + item.Value.Address);
+                Console.WriteLine("City: " + item.Value.City);
+                Console.WriteLine("State: " + item.Value.State);
+                Console.WriteLine("Zip: " + item.Value.Zip);
+                Console.WriteLine("Phone Number: " + item.Value.PhoneNumber);
+                Console.WriteLine("Email: " + item.Value.Email);
+            }
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,6 +7,9 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book Problem!");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact("Faiz", "Anwar", "Hospital Chowk", "Daltonganj", "Palamau", 822101, 9876543210, "faiz.anwar@gmail.com");
+            addressBook.ViewContact();
             Console.ReadLine();
         }
     }
