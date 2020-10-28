0ac6bacddbba0eeffccc5e6253e16f8968927652
Faiz <faiz.anwar4749@gmail.com>
2020-10-28 15:02:45 +0530

    [Faiz] Add: Method to Edit an existing contact

--- AddressBook/IContacts.cs
+++ AddressBook/IContacts.cs
@@ -6,5 +6,6 @@ namespace AddressBook
     interface IContacts
     {
         public void AddContact(string firstName, string lastName, string address, string city, string state, int zip, string phoneNumber, string email);
+        public void EditContact(string name);
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -8,23 +8,25 @@ namespace AddressBook
         {
             Console.WriteLine("Welcome to the Address Book Problem!");
             AddressBook addressBook = new AddressBook();
-            Console.WriteLine("Enter First Name: ");
+            Console.Write("Enter First Name: ");
             string firstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name: ");
+            Console.Write("Enter Last Name: ");
             string lastName = Console.ReadLine();
-            Console.WriteLine("Enter Address: ");
+            Console.Write("Enter Address: ");
             string address = Console.ReadLine();
-            Console.WriteLine("Enter City: ");
+            Console.Write("Enter City: ");
             string city = Console.ReadLine();
-            Console.WriteLine("Enter State: ");
+            Console.Write("Enter State: ");
             string state = Console.ReadLine();
-            Console.WriteLine("Enter ZIP: ");
+            Console.Write("Enter ZIP: ");
             int zip = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter phNo: ");
+            Console.Write("Enter phNo: ");
             string phoneNumber =Console.ReadLine();
-            Console.WriteLine("Enter email: ");
+            Console.Write("Enter email: ");
             string email = Console.ReadLine();
             addressBook.AddContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            addressBook.ViewContact();
+            addressBook.EditContact(firstName);
             addressBook.ViewContact();
             Console.ReadLine();
         }
